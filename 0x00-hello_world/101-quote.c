822ad3dc5c13315da24382fd003cc80534f11b0b
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-16 20:59:51 -0700

    alx low level programmingA

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 12);
-return (0);
+fputs("and that piece of art is useful" "- Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
