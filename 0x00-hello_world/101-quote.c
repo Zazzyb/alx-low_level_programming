b8eb96f19824da32ae815b9ad35eed14a1d3563e
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-16 20:41:26 -0700

    alx low level programmingA

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
 * main - Emtry Point
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-sprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 12);
+return (0);
 }
