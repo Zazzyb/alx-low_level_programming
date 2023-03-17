02a8e68ecba842d245321dac7a9cc9a8c1003108
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-16 17:08:03 -0700

    alx low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main(void)
 {
-/* initialize string*/
-puts(" "Programming is like building a multilingual puzzle");
+/*"puts" also prints strings*/
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
