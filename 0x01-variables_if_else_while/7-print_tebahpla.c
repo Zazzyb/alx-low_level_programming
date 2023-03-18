9c339470f57bce13dfc73da93336605dbf2e746b
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-17 18:25:21 -0700

    variables _if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch[26] = "zyxwvutsrqponmlkjihgfedcba";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(ch[i]);
+}
+putchar('\n');
+return (0);
+}
