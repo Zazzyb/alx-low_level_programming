669f45a9286b7eeb4193352c5215a0f5bad30614
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-17 17:32:57 -0700

    C Variables if else_do while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch[10] = "0123456789";
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(ch[i]);
+}
+putchar('\n');
+return (0);
+}
