84c0e07ac0211e7cce56ded8124680494000e8e2
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-21 15:57:35 +0100

    function_nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*main - check the code
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+int i;
+for (i = 97; i <= 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
