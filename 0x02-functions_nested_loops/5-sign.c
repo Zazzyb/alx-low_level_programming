1517029b8dc6b9af8e0a77d0f89343aee9e05f4f
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-21 18:19:35 +0100

    function_nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_sign - print the sign of a number
+*@n: character to be pointed
+*Return: 1 and print + if n is greater than zero,
+*else zero(0) if zero, otherwise -1 and prints - if n is less than zero
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+}
+return (1);
+else if (n == 0)
+{
+_putchar('0');
+}
+return (0);
+else
+{
+_putchar('-')
+}
+return (-1);
+}
