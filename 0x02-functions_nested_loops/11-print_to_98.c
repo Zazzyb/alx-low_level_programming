8273c27d75c7d007f48f1022ac350c26bc4fc768
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-22 05:16:16 +0100

    function_nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,16 +6,17 @@
 */
 void print_to_98(int n)
 {
+int i;
 if (n < 98)
 {
-for (n <= 0; n < 98; n++)
+for (n = i; n < 98; n++)
 {
 _putchar(n);
 }
 }
 else if (n > 98)
 {
-for (n >= 98; n > 98; n--)
+for (n = i; n > 98; n--)
 {
 _putchar(n);
 }
