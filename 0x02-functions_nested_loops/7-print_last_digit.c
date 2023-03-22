f419db5807a6a53f203c4d7bd807432e00b837d1
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-22 02:32:20 +0100

    function_nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*print_last_digit - prints last digit of a number
+*@i: character where the value ppints to
+*Return: Always 0.
+*/
+int print_last_digit(int i)
+{
+int r;
+r = i % 10;
+return (r);
+}
