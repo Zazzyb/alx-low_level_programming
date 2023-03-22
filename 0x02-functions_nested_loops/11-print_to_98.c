3e31d0dd1204e9136c53f39a95d96ccbdf952146
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-22 05:45:02 +0100

    function_nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,18 @@
 void print_to_98(int n)
 {
 int i;
-if (n < 98)
+if (i < 98)
 {
 for (i = n; i < 98; i++)
 {
-printf("%d", i);
+printf("%d, ", i);
 }
 }
 else
 {
-for (i = n; i > 98; i--)
+for (i = n; i >= 98; i--)
 {
-  printf("%d", i);
+  printf("%d, ", i);
 }
 }
 printf("\n");
