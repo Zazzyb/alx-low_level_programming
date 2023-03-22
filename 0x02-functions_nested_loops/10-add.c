14819aa4384dca5138361cb35895a0c75c618818
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-22 04:09:29 +0100

    function_nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - adds to integers
+ *@a: value to be added
+ *@b: value to be added
+ *Return: Always 0.
+ */
+int add(int a, int b)
+{
+int r;
+r = a + b;
+return (r);
+}
