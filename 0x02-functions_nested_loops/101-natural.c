fd4b7ed5495c5b2b62f997cdf66afd4a35c1e36f
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-23 03:54:11 +0100

    function_nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+int i, j = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+j += i;
+}
+printf("%d\n", j);
+return (0);
+}
