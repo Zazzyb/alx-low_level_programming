067bb4e9a31903ff6959dbb289fe38ed002213c1
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-24 05:57:15 +0100

    project on more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - check for uppercase character
+*@a: variable declared
+*@b: variable declared
+*Return: addition of multiplication.
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
