bf5aa30ca67923fc7742aea0a3e23e0ab27c57f6
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-24 04:46:13 +0100

    project on more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_isupper - check for uppercase character
+*@c: variable declared
+*Return: 0 or 1.
+*/
+int _isupper(int c)
+{
+int i;
+if (c == i)
+{
+for (i = 65; i < 90; i++)
+return (1);
+}
+else
+{
+return (0);
+}
+}
