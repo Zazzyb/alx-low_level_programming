f22240c984c4d1a714ede391241e0364628b1e4b
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-25 04:18:37 +0100

    project on more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_number - print integer
+*@n: variable declared
+*Return: Always 0.
+*/
+void print_number(int n)
+{
+unsigned int i = n;
+if (n < 0)
+{
+_putchar('_');
+i = -i;
+}
+if ((i / 10) > 0)
+print_number(i / 10);
+_putchar((i % 10) + 48);
+}
+
