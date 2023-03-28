071f0931bfaf4d50a4062be5ed43b725adac2548
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-28 06:27:34 +0100

    C-pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_array - function that prints elements of an array
+*@a: parameter to print
+*@n: parameter to print
+*Return: Always 0
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < (n - 1); i++)
+{
+printf("%d", a[i]);
+}
+if (i == (n - 1))
+{
+printf("%d", a[n - 1]);
+}
+printf("\n");
+}
