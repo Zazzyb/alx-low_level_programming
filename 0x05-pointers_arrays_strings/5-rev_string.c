fc04f17152fcc41ff9f25b7ad9376a50cc375d34
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-28 06:15:08 +0100

    C-pointers_arrays_strings project

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,11 +6,10 @@
 */
 void rev_string(char *s)
 {
-char r;
+char r = s[0];
 int c = 0;
 int i;
 while (s[c] != '\0')
-{
 c++;
 for (i = 0; i < c; i++)
 {
@@ -18,6 +17,5 @@ c--;
 r = s[i];
 s[i] = s[c];
 s[c] = r;
-}
 }
 }
