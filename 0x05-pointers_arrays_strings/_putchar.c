0f65b95dd429a4a39a35b5c24529fe633ac59087
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-28 05:16:29 +0100

    C-pointers_arrays_strings project

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <unistd.h>
 /**
-*_purchar - writes the character c to stdout
+*_putchar - writes the character c to stdout
 *@c: character to print
 *
-*Rerurn: on success 1
+*Return: on success 1
 *on error, -1 is returned, and errno is set appropriately
 */
 int _putchar(char c)
