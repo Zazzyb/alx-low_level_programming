2dfeb8410d95fae26abdcc032b7ceafa68c4d899
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-28 04:28:13 +0100

    C-pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strlen - returns the length of a string
+*@s: parameter to return
+*Return: Always 0
+*/
+int _strlen(char *s)
+{
+int i = 0;
+while (*s != '\0')
+{
+i++;
+s++;
+}
+return (i);
+}
