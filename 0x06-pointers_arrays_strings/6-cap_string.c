d76ad3917a319a344a8ff237d3efb93a837fda2b
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-30 04:48:22 +0100

    C- More pointers, arrays and strings project

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*cap_string - all letters uppercase
+*@c: first variable
+*Return: c
+*/
+char *cap_string(char *c)
+{
+int x;
+x = 0;
+while (c[x])
+{
+while (!(c[x] >= 'a' && c[x] <= 'z'))
+x++;
+if (c[x - 1] == ' ' || c[x - 1] == '\t' || c[x - 1] == '\n' || c[x - 1] == ',')
+if (c[x - 1] == ';' || c[x - 1] == '.' || c[x - 1] == '!' || c[x - 1] == '?')
+if (c[x - 1] == '"' || c[x - 1] == '(' || c[x - 1] == ')' || c[x - 1] == '{')
+if (c[x - 1] == '}' || x == 0)
+c[x] -= 32;
+x++;
+}
+return (c);
+}
