891bea2d6e43ac46260da592f28f73da62cedf3f
Ebibinitei <zazzy_b27@yahoo.com>
2023-03-30 05:08:07 +0100

    C- More pointers, arrays and strings project

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*leet - encodes a string into 1337
+*@c: first variable
+*Return: c
+*/
+char *leet(char *c)
+{
+int x, y;
+char s1[] = "aAeEoOrTlL";
+char s2[] = "4433007711";
+for (x = 0; c[x] != '\0'; x++)
+{
+for (y = 0; y < 10; y++)
+{
+if (c[x] == s1[y])
+{
+c[x] = s2[y];
+}
+}
+}
+return (0);
+}
