863d35bd17631e47f5623196fe1673d1da16993d
Ebibinitei <zazzy_b27@yahoo.com>
2023-04-06 04:47:41 +0100

    C projects on Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_pow_recursion - power of a value
+*@x: variable to print
+*@y: variable for power
+*Return: value of x raised to the power of y
+*if y is less than 0, return -1
+*/
+int _pow_recursion(int x, int y)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+return (n * factorial(n - 1));
+}
+}
